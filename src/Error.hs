9b87a5c0e3d4360adda0be6267a71bb3f44a12ca
Sarah Ohlin <sarahohlin1@gmail.com>
2019-09-23 11:40:13 -0400

    more small implementation steps

--- src/Error.hs
+++ src/Error.hs
@@ -3,7 +3,8 @@ module Error
 ) where
     
 import Prelude hiding (Left, Right)
-import Graphics.Image
+import Graphics.Image hiding (Direction)
+import RoundPixel
 
 -- | Calculates the RGB error to add to a given pixel position.
 errorOnto :: Image VS RGB Double -> (Int, Int) -> (Double, Double, Double)
@@ -11,3 +12,16 @@ errorOnto img (0, 0) = (0, 0, 0)
 errorOnto img (x, y)
     | x < 0 = (0, 0, 0)
     | y < 0 = (0, 0, 0)
+    | maybeIndex img (x, y) == Nothing = (0, 0, 0)
+errorOnto img (x, y) =
+
+-- | All possible directions where error can come from.
+data Direction = Left | TopLeft | Above | TopRight
+-- | The fraction of error of the pixel in the given direction to apply to the current pixel
+errorFrac :: Direction -> Double
+errorFrac dir =
+    case dir of
+        Left -> 7/16
+        TopLeft -> 1/16
+        Above -> 5/16
+        TopRight -> 3/16
